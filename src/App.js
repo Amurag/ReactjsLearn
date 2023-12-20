4d884b95aa083a7fca06f1c7cc435ad091fc0169
Amung <anurag.thakur@queenzend.com>
2023-12-20 18:54:32 +0530

    props

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Card from './components/card';
+
+
+
 
 function App() {
+  let myObj={
+    name:"Anurag", age:"24"
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<>
+<Card Channel="ChaiaurCode" someobj={myObj} btnText="click me"/>
+<Card Channel="Anurag" btnText="more"/>
+<Card Channel="Anurag"/>
+</>
   );
 }
 
