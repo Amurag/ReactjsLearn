fa9ed9c287881e39a0a610e27fb39ee327e171eb
Amung <anurag.thakur@queenzend.com>
2023-12-26 12:15:44 +0530

    clour change

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
 
+import { useState } from 'react';
 import './App.css';
-import Card from './components/card';
+
 
 
 
 
 function App() {
-  let myObj={
-    name:"Anurag", age:"24"
-  }
+const[color ,setcolor]=useState("olive")
   return (
 <>
-<Card Channel="ChaiaurCode" someobj={myObj} btnText="click me"/>
-<Card Channel="Anurag" btnText="more"/>
-<Card Channel="Anurag"/>
+<div className=' w-full h-screen duration-200' style={{backgroundColor:color}}>
+  <div className=' fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
+<div className=' flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-xl'>
+  <button onClick={()=>setcolor("red")} className=' outline-none px-4' style={{background:"red"}}>Red</button>
+  
+</div>
+<div className=' flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-xl'>
+<button  onClick={()=>setcolor("green")} className=' outline-none px-4' style={{background:"green"}}>Green</button>
+  
+</div>
+
+  </div>
+</div>
 </>
   );
 }
